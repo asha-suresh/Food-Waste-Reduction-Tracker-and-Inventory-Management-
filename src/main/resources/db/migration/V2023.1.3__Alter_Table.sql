503ab5d5d61128c9738ad59fada13b3ccbd19873
ASHA SURESH <chikkuasha915@gmail.com>
2023-08-20 21:58:50 +0100

    modification

--- src/main/resources/db/migration/V2023.1.3__Alter_Table.sql
+++
@@ -1,3 +0,0 @@
-alter table food_info add column if not exists category varchar(15);
-
-alter table inventory add column if not exists category varchar(15);
--- src/main/resources/db/migration/V2023.1.7__updating_collection_tables.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE collections
-ADD collection_name varchar(40);
-
-ALTER TABLE collections
-ADD updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
